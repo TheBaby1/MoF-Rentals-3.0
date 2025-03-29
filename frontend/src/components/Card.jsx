c0c0b3248961a2eda1bf8fad1c9da956c88150b6
Nikko <nikkonuriel@gmail.com>
2025-03-30 02:19:14 +0800

    added features

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,14 +1,35 @@
 import styles from '../modules/Card.module.css';
+import { useEffect, useState } from 'react';
 function Card(){
+
+    const [ models, setModels ] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/models')
+            .then((res) => res.json())
+            .then((data) => setModels(data))
+            .catch((error) => console.error("Error Fetching Rentals: ", error))
+    }, []);
+
+    function clicked(){
+        console.log('Clicked!');
+    }
 
     return (
         <>
-            <div className={styles.card}>
-                <img className={styles.cardImage}></img>
-                <h2 className={styles.cardName}>Marcelo</h2>
-                <h3 className={styles.cardPrice}>110</h3>
-                <p className={styles.cardText}>This one is the best</p>
-            </div>
+            {models.length > 0 ? (
+                models.map((model) => (
+                    <div className={styles.card} key={model._id}>
+                        <img className={styles.cardImage} alt="Model"></img>
+                        <h2 className={styles.cardName}>{model.firstName}</h2>
+                        <h3 className={styles.cardPrice}>Price: {model.price}</h3>
+                        <p className={styles.cardText}>{model.description}</p>
+                        <button className={styles.button} onClick={clicked}>Rent</button>
+                    </div>
+                ))
+            ) : (
+                <p>Loading rentals...</p>
+            )}
         </>
     );
 }
